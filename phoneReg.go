3118872731e6dc71081e1ae2ee020074bee8d2ab
Vania Kraich <vaniakraich@gmail.com>
2024-04-21 17:35:31 +0300

    -v 1.0

--- phoneReg.go
+++ phoneReg.go
@@ -2,22 +2,20 @@ package phoneRegPackage
 
 import (
 	"regexp"
-	"strconv"
 )
 
 var (
 	phoneReg = regexp.MustCompile(`(\+\d{1,2}\s?)?(\(\d{3}\)|\d{3})[\s\.-]?\d{3}[\s\.-]?\d{4}`)
 )
 
-// Function accepts a slice of numbers, among then it looks for phone numbers
-// Returns also a slice from an integer
+// PhoneReg takes a slice of strings and checks each string for phone numbers.
+// Returns a new string slice containing only the phone numbers found.
 
-func PhoneReg(slice []int) []int {
-	var phonesSlice []int
+func PhoneReg(slice []string) []string {
+	var phonesSlice []string
 
 	for _, val := range slice {
-		strVal := strconv.Itoa(val)
-		isValid := phoneReg.MatchString(strVal)
+		isValid := phoneReg.MatchString(val)
 		if isValid {
 			phonesSlice = append(phonesSlice, val)
 		}
